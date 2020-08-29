f7c7f69686a26142710f4ec5527fcd92f6ad0124
SmileMagaX <70013279+SmileMagaX@users.noreply.github.com>
2020-08-29 10:24:21 +0700

    Create Exercise4_Adiluck_S.py
    การบ้าน

---
+++ Exercise4_Adiluck_S.py
@@ -0,0 +1,12 @@
+f = (120)
+g = (200)
+i = (55)
+c = (78)
+
+
+
+print("---Your Score---")
+print("Foundation English                   :", (f),"point")
+print("General Business                     :", (g),"point")
+print("Introduction to Computer Systems     :", (i),"point")
+print("Computer Programming                 :", (c),"point")
